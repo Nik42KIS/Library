33a82b34340b6513a0829348d31ee6e5e4fb0877
Nikita K <rabot422@gmail.com>
2023-10-12 15:43:05 +0300

    made add to library

--- app.js
+++ app.js
@@ -1,34 +1,33 @@
-
-const books = document.querySelector('.books')
-const modal = document.querySelector('.modal')
-const form =  document.querySelector('.modal__form')
-const modalBtn = document.querySelector('.modal_btn')
-const inputTitle = document.getElementById('input_title')
-const inputAuthor = document.getElementById('input_author')
-const inputPages = document.getElementById('input_pages')
+const books = document.querySelector('.books');
+const modal = document.querySelector('.modal');
+const form = document.querySelector('.modal__form');
+const modalBtn = document.querySelector('.modal_btn');
+const inputTitle = document.getElementById('input_title');
+const inputAuthor = document.getElementById('input_author');
+const inputPages = document.getElementById('input_pages');
 
 const library = [
-    {
-        title : 'Kerry',
-        author: 'S.King',
-        pages: 250,
-    }
-]
+  {
+    title: 'Kerry',
+    author: 'S.King',
+    pages: 250,
+  },
+];
 
-function Book(title, author, pages){
-
-this.title = title;
-this.author = author;
-this.pages = pages;
+function Book(title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
 }
 
-modalBtn.addEventListener('click', addBookToLibrary )
+modalBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  addBookToLibrary();
+});
 
-function addBookToLibrary(){
- library.push(new Book(inputTitle.textContent, inputAuthor.textContent, inputPages.textContent))
- console.log(library)
+function addBookToLibrary() {
+  library.push(new Book(inputTitle.value, inputAuthor.value, inputPages.value));
+  console.log(library);
 }
 
-let book1 = new Book('h', 's', 't')
-
-console.log(library)
+console.log(library);
