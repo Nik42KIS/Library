d0ba5af2a1761ac1638957cae1347daeccaf107b
Nikita K <rabot422@gmail.com>
2023-10-12 16:07:42 +0300

    finish with output book on the page

--- app.js
+++ app.js
@@ -5,19 +5,41 @@ const modalBtn = document.querySelector('.modal_btn');
 const inputTitle = document.getElementById('input_title');
 const inputAuthor = document.getElementById('input_author');
 const inputPages = document.getElementById('input_pages');
+const inputRead = document.getElementById('input_read');
 
 const library = [
-  {
-    title: 'Kerry',
-    author: 'S.King',
-    pages: 250,
-  },
+//   {
+//     title: 'Kerry',
+//     author: 'S.King',
+//     pages: 250,
+//   },
 ];
-
-function Book(title, author, pages) {
+// create constructor for new Books
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
+  this.read = read;
+}
+
+// render books from library 
+function render() {
+    const newBook = document.createElement('div');
+  library.map((i) => {
+    
+    newBook.innerHTML = `
+        <div class="books__item">
+        <div class="item__title">${i.title}</div>
+        <div class="item__author">${i.author}</div>
+        <div class="item__pages">${i.pages}</div>
+        <div class="item__read">
+            <input type="checkbox" checked>
+            <input type="checkbox">
+        </div>
+    </div>
+        `;
+    books.appendChild(newBook);
+  });
 }
 
 modalBtn.addEventListener('click', (e) => {
@@ -25,9 +47,11 @@ modalBtn.addEventListener('click', (e) => {
   addBookToLibrary();
 });
 
+
 function addBookToLibrary() {
   library.push(new Book(inputTitle.value, inputAuthor.value, inputPages.value));
   console.log(library);
+  render();
 }
 
 console.log(library);
