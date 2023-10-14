680deb95ce10565e9cf9866123c2833a524b8a04
Nikita K <rabot422@gmail.com>
2023-10-14 13:56:15 +0300

    add fitcha witch remove book to click on the button

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ const inputPages = document.getElementById('input_pages');
 const inputRead = document.getElementById('input_read');
 const openModal = document.querySelector('.open_modal_btn');
 const closeModal = document.querySelector('.modal_close');
-const deleteBtn = document.querySelector('.delete_btn');
+
 
 const library = [
-    // {
-    //   title: 'Kerry',
-    //   author: 'Steven King',
-    //   pages: '243'
-    // }
+    {
+      title: 'Kerry',
+      author: 'Steven King',
+      pages: '243'
+    }
 ];
 
 openModal.addEventListener('click', () =>{ 
@@ -37,33 +37,32 @@ function Book(title, author, pages) {
 // render books from library 
 function render() {
   
-    const newBook = document.createElement('div');
-  library.map((i) => {
-    
-    newBook.innerHTML = `
-        <div class="books__item">
-          <div class="item__title">${i.title}</div>
-          <div class="item__author">${i.author}</div>
-          
-          <div class="container_for_active">
-          <div class="item__pages">pages:${i.pages}</div>
-          <div class="read_status">
-          <span>Read</span>
-           <input type="radio" name="status" id=""> <br>
-           <span>Not read</span>
-           <input type="radio" name="status" checked id="">
-      </div>
-     <button class="delete_btn"> 
-       <img src="./images/trash.png" alt="delete">
-     </button>
-     </div>
-        `;
-    books.appendChild(newBook);
+  const newBook = document.createElement('div');
+  newBook.classList.add('books__item')
+library.map((i) => {
+  
+  newBook.innerHTML = `
+        <div class="item__title">${i.title}</div>
+        <div class="item__author">${i.author}</div>
+        
+        <div class="container_for_active">
+        <div class="item__pages">pages:${i.pages}</div>
+        <div class="read_status">
+        <span>Read</span>
+         <input type="radio" name="status" id=""> <br>
+         <span>Not read</span>
+         <input type="radio" name="status" checked id="">
+    </div>
+   <button class="delete_btn"> 
+     <img src="./images/trash.png" alt="delete">
+   </button>
+      `;
+  books.appendChild(newBook);
 
- 
-  });
+
+});
 };
-
+render()
 //delete book
 
 
@@ -80,6 +79,7 @@ modalBtn.addEventListener('click', (e) => {
 
 
 closeModal.addEventListener('click', (e) => {
+ 
   e.preventDefault();
   modal.close()
 });
@@ -89,11 +89,18 @@ function addBookToLibrary() {
   library.push(new Book(inputTitle.value, inputAuthor.value, inputPages.value));
   console.log(library);
   render();
+  const deleteBtn = document.querySelectorAll('.delete_btn');
+  deleteBtn.forEach((item) =>{
+    console.log(item)
+    item.addEventListener('click', (e)=>{
+      console.log(e.target)
+      e.target.parentElement.parentElement.parentElement.remove()
+      })
+  })
+ 
+ 
 }
 
-deleteBtn.addEventListener('click', (e)=>{
-  console.log(e.target)
-  e.target.parentElement.parentElement.parentElement.remove()
-  })
+
 
 console.log(library);
